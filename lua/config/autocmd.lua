eeeb337599a257f3b360b753c9fe4a4d9f1aa1a8
Yaron Horsky <yaronh@moveo.co.il>
2025-11-26 19:26:19 +0200

    added script for updating date in front matter when saving

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -16,3 +16,23 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<cmd>q<CR>", { buffer = true })
   end,
 })
+
+local function update_lastmod()
+  local ts = os.date("%Y-%m-%dT%H:%M:%S%z"):gsub("(%d%d)(%d%d)$", "%1:%2")
+
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  for i, line in ipairs(lines) do
+    if line:match("^lastmod%s=") then
+      lines[i] = "lastmod = " .. ts
+      break -- stop after first match
+    end
+  end
+
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+end
+
+-- keymap for updating the front matter lastmod field to the saving time
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.md", "*.markdown" }, -- adjust patterns as you like
+  callback = update_lastmod,
+})
